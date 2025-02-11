5888812cf7adfdd1331217165f8e60a019b9d9aa
008 <xeroxero8@outlook.com>
2025-02-11 23:14:11 +0600

    chore: updated hero section

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router'
-import Layout from './layout/Layout.jsx'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Blog from './pages/Blog.jsx'
-import ContactUs from './pages/ContactUs.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./layout/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Blog from "./pages/Blog.jsx";
+import ContactUs from "./pages/ContactUs.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-          <Route element={<Layout/>}>
-            <Route path='/' element={<Home/>} ></Route>
-            <Route path='/about' element={<About/>}></Route>
-            <Route path='/blog' element={<Blog/>}></Route>
-            <Route path='/contact' element={<ContactUs/>}></Route>
-            </Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/blog" element={<Blog />}></Route>
+          <Route path="/contact" element={<ContactUs />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
